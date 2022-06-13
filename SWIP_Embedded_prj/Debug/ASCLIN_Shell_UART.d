911730af0a0724a35f55b38d461d11bbb5754583
hcyoon <yhc2050@gmail.com>
2022-06-14 07:52:25 +0900

    20220614 Delete Debug

--- SWIP_Embedded_prj/Debug/ASCLIN_Shell_UART.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-ASCLIN_Shell_UART.o :	../ASCLIN_Shell_UART.c
-../ASCLIN_Shell_UART.c :
-ASCLIN_Shell_UART.o :	../ASCLIN_Shell_UART.h
-../ASCLIN_Shell_UART.h :
--- SWIP_Embedded_prj/Debug/light_sensor.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-light_sensor.o :	../light_sensor.c
-../light_sensor.c :
-light_sensor.o :	../light_sensor.h
-../light_sensor.h :
